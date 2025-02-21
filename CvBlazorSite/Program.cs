9c9a2413c08a7f48bb32f807af34922b47be9a6a
HolyAcorn <74989722+HolyAcorn@users.noreply.github.com>
2025-02-21 13:14:17 +0100

    Admin login

--- CvBlazorSite/Program.cs
+++ CvBlazorSite/Program.cs
@@ -33,7 +33,7 @@ namespace CvBlazorSite
                 })
                 .AddIdentityCookies();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = Environment.GetEnvironmentVariable("CvDbConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
